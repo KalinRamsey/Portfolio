b6a7e151834d669f159373338758c81487c4834e
Kalin Ramsey <kalinramsey@gmail.com>
2020-03-24 16:22:23 -0700

    addressed HTML validation error in mailto: attr - replaced spaces with %20. added favicon. created jQuery script to dynamically change subject and body message auto-loaded in users email client based on user input. added an email thumbnail with (mailto:) attr in footer section along with linkedin and github links. all links now open in new tabs.

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function main(){
     // Load each section on corresponding button click
     handleContentDisplay();
+    handleContactSubmission();
+}
+
+function handleContactSubmission (){
+    $('#contactForm').submit(e => {
+        e.preventDefault();
+
+        let subject = e.target.subject.value;
+        let actionSubject = subject.split(' ').join('%20');
+        // console.log(actionSubject);
+
+        let message = e.target.message.value;
+        let actionMessage = message.split(' ').join('%20');
+        // console.log(actionMessage);
+        
+        let actionQuotes = "mailto:kalinramsey@gmail.com?subject=" + actionSubject + "&body=" + actionMessage;
+        if (e.target.phone.value){
+            actionQuotes += "%20\nCall%20Me%20at%20" + e.target.phone.value;
+        }
+        console.log(actionQuotes);
+
+        $('#contactForm').attr('action', `${actionQuotes}`);
+        
+        e.currentTarget.submit();
+    });
 }
 
 function handleContentDisplay() {
